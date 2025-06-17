0f4d498929170d4eff99050ff487d86ef39af7e4
Sadukie <sarah@cletechconsulting.com>
2025-06-16 21:51:24 -0400

    Updating the timer for CodeMash 2026 CFP countdown

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ def format_date(dateToFormat):
     return f"{dateToFormat:{fmt}}"
 
 async def countdown():
-    codemash_date = datetime(2025,1,14,8,0,0)
+    ending_date = datetime(2025,8,15,0,0,0)
+    event_name = "the CodeMash 2026 CFP Opening"
+    display(event_name, target="eventName")
+    display(format_date(ending_date),target="endingDate",append=False)
     while True:
         await asyncio.sleep(1)
-        td = (codemash_date - datetime.now())
+        starting_date = datetime.now()
+        td = ending_date - starting_date
         days, hours, minutes, seconds = td.days, td.seconds // 3600, td.seconds // 60 % 60, (td.seconds % 60)
-        display(format_date(codemash_date),target="codemashDate",append=False)
         display(days, target="days", append=False)
         display(hours, target="hours", append=False)
         display(minutes, target="minutes", append=False)
         display(seconds, target="seconds", append=False)
+        td = ending_date - starting_date
+
 
 
 await countdown()
